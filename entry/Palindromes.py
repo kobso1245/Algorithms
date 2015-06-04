213f9348c54f128d153fd6d76eb4575bd6e7273e
Kaloyan <kalo.evt@mail.bg>
2015-06-04 12:52:36 +0300

    Done with Palindromes

---
+++ entry/Palindromes.py
@@ -0,0 +1,25 @@
+def is_pallindrome(value):
+    length = len(value)
+    half_length = length // 2
+    for i in range(half_length):
+        if value[i] != value[length - i - 1]:
+            return False
+
+    return True
+
+def generate_all_combos(input_word):
+    resulted_combos = [input_word]
+    length = len(input_word)
+    for i in range(1, length):
+        resulted_combos.append(input_word[i:]+input_word[:i])
+
+    return resulted_combos
+
+
+#function to run
+def check_for_palindromes(input_word):
+    combos = generate_all_combos(input_word)
+    result = [word for word in combos if is_pallindrome(word)]
+    if result == []:
+        result = 'NONE'
+    return result
